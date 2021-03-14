8ffe04b39e76ea84a45a6030579ab5ea9ddbfbbc
Daniel Bejarano <dbejarano820@gmail.com>
2021-03-13 20:05:31 -0600

    nada

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
 
+
+void algortimo(int pLargo, int pAlto){
+
+    printf("void setup()\n{\n\t size(%d, %d);\n\t background(100);\n}\n", pLargo, pAlto);
+
+  //  printf("")
+    //printf
+
+}
+
+
 int main(){
 
-    cout<< "Hola mundo" << endl;
+    algortimo(50, 50);
 
     return 0;
 }
